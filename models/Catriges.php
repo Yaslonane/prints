2d1dbf58921f47720e641b8d5bc7badd31eeab93
Andrey <yaslonane@yandex.ru>
2016-10-17 21:33:44 +0300

    +добавление модели картриджей

---
+++ models/Catriges.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Catriges
+ *
+ * @author andrey
+ */
+class Catriges {
+    //put your code here
+    
+    public static function getSumAllCarteiges(){ //получаем сумму всех картриджей(доставленных и замененных) по запросу ниже
+        
+        $sql = "SELECT c.name as name, sum(aC.value) as value FROM actionsCartriges aC
+                INNER JOIN cartriges c ON aC.id_cartriges = c.id
+                GROUP BY c.name;";
+        
+        return arr();
+    }
+    
+    public static function addCartriges(){ //добавление действия с картриджеми
+        
+        
+    }
+    
+    //public static function RemoveCartriges()
+    
+    public static function AddNewCartriges(){ // добавление нового картриджа
+        
+        
+    }
+    
+    public static function ReactivCartriges(){ // изменение статуза задействования принтера
+        
+        
+    } 
+    
+}
